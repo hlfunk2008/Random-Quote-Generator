1510ef63407421865fc4d3b3c19c935b6aeb13d0
Hunter Funk <hlfunk2008@gmail.com>
2026-03-02 11:31:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,21 +12,30 @@ function getRandomQuote() {
   return quotes[randomNumber]
 }
 
-// Replaces the quote on screen with the randomly selected quote
+
 function printQuote() {
   let quote = `<p class="quote-">${getRandomQuote().quote}</p>`
   let source = `<p class="source">${getRandomQuote().source}`
 
+
+  // detects 
   if (getRandomQuote().quote !== "") {
     document.getElementsByClassName('quote')[0].innerHTML = `${getRandomQuote().quote}`
   }
-
   if (getRandomQuote().citation !== "") {
     document.getElementsByClassName('source')[0].innerHTML = ` ${getRandomQuote().citation}`
   } if (getRandomQuote().year !== "") {
     document.getElementsByClassName('source')[0].innerHTML += ` ${getRandomQuote().year}`
   }
+  updateBgColor()
 }
 
+function updateBgColor() {
+  let r1 = Math.floor( Math.random() * 255);
+  let r2 = Math.floor( Math.random() * 255);
+  let r3 = Math.floor( Math.random() * 255);
+
+  document.getElementById('body').setAttribute(b)
+}
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
