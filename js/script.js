bf7031b8226182020b20d1e719ee0e57d321f3b6
Hunter Funk <hlfunk2008@gmail.com>
2026-03-02 11:21:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+let quotes = [
+  {"quote": "one", "source": "t1", "citation": "Twitter", "year": "2011"},
+  {"quote": "two", "source": "t2", "citation": "Youtube", "year": "2000"},
+  {"quote": "three", "source": "t3", "citation": "Reddit", "year": "210 BCE"},
+  {"quote": "four", "source": "t4", "citation": "Ancient Sumerian Tablet", "year": "2026"},
+  {"quote": "five", "source": "t5", "citation": "Newspaper", "year": "2"}
+]
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Gets a random number and returns an object containing quotes, citations, etc.
+function getRandomQuote() {
+  let randomNumber = Math.floor( Math.random() * 5);
+  return quotes[randomNumber]
+}
 
-/*** 
- * `quotes` array 
-***/
+// Replaces the quote on screen with the randomly selected quote
+function printQuote() {
+  let quote = `<p class="quote-">${getRandomQuote().quote}</p>`
+  let source = `<p class="source">${getRandomQuote().source}`
 
+  if (getRandomQuote().quote !== "") {
+    document.getElementsByClassName('quote')[0].innerHTML = `${getRandomQuote().quote}`
+  }
 
+  if (getRandomQuote().citation !== "") {
+    document.getElementsByClassName('source')[0].innerHTML = ` ${getRandomQuote().citation}`
+  } if (getRandomQuote().year !== "") {
+    document.getElementsByClassName('source')[0].innerHTML += ` ${getRandomQuote().year}`
+  }
+}
 
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
